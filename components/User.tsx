e16c8c3792ecae521e54a739216e0d297c3a7a80
Sourav Sanjay <souravsanjay07@gmail.com>
2025-08-08 11:58:52 +0530

    Added types to component props

---
+++ components/User.tsx
@@ -0,0 +1,17 @@
+
+type UserProps = {name: string; age: number}; // Second method for defining types for props
+interface UserPropsInterface { //Third method for defining types for props
+    name: string;
+    age: number;
+}
+
+const User = ({name, age}: {name: string; age: number}) => { //First method for defining types for props
+  return (
+    <div>
+        <h1>{name}</h1>
+        <h1>{age}</h1>
+    </div>
+  )
+}
+
+export default User
