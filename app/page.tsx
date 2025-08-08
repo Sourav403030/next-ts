b9829e02f56b652b1ac66657acba687ddc5c7275
Sourav Sanjay <souravsanjay07@gmail.com>
2025-08-08 13:07:51 +0530

    Added types to useState hook

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import ChildrenTypes from '@/components/ChildrenTypes'
 import ComponentTypes from '@/components/ComponentTypes'
 import ReusableUser from '@/components/ReusableUser'
 import ReusableAdmin from '@/components/ReusableAdmin'
+import UseStateTypes from '@/components/UseStateTypes'
 
 import React from 'react'
 
@@ -19,6 +20,9 @@ const page = () => {
       <ReusableUser name="Alex" age={20} email="alex@gmail.com" location={["NewYork", "USA"]} />
 
       <ReusableAdmin name="Willi" age={25} email="willi@gmail.com" location={["Manchester", "England"]} admin="yes" />
+      <br/>
+
+      <UseStateTypes/>
     </main>
   )
 }
---
+++ components/UseStateTypes.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React, { useState } from 'react'
+
+const UseStateTypes = () => {
+    const [count, setCount] = useState<number>(0);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+
+    </div>
+  )
+}
+
+export default UseStateTypes
